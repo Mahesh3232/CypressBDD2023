2c749acd02bc98d241c4419e9e3e5b14639c380e
Mahesh3232 <mahesh.aher1999@gmail.com>
2023-05-17 21:05:23 +0530

    BDD login

--- cypress/integration/Login/Login.spec.js
+++ cypress/integration/Login/Login.spec.js
@@ -1,10 +1,10 @@
-import { Given, Then, When } from "cypress-cucumber-preprocessor/steps";
+import { Given, Then, When , And} from "cypress-cucumber-preprocessor/steps";
 
 Given('I open sauce demo Url',()=>{
     cy.visit('https://www.saucedemo.com/')
 })
 
-Then('Fill the Username',()=>{
+And('Fill the Username',()=>{
     cy.get('#user-name').type('standard_user')
 })
 
@@ -15,3 +15,28 @@ Then('Fill the password',()=>{
 Then('Click on login Button',()=>{
     cy.get('#login-button').click()
 })
+
+And('Validate sucessful login.',()=>{
+    cy.get('.header_secondary_container > span').should('be.visible').and('have.text','Products')
+})
+
+
+Given('I open sauce demo Url.',()=>{
+    cy.visit('https://www.saucedemo.com/')
+})
+
+When('Fill the Username.',()=>{
+    cy.get('#user-name').type('standard_user')
+})
+
+And('Fill the password.',()=>{
+    cy.get('#password').type('secret_sauce12345')
+})
+
+Then('Click on login Button.',()=>{
+    cy.get('#login-button').click()
+})
+
+// And('Validate sucessful login.',()=>{
+//     cy.get('.header_secondary_container > span').should('be.visible').and('have.text','Products')
+// })

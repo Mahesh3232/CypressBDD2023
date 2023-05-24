7539b45a136b6d7e22966d327849175f43d896ab
Mahesh3232 <mahesh.aher1999@gmail.com>
2023-05-24 21:10:46 +0530

    Ecommerce

--- cypress/integration/AddToCart/AddToCakrt.spec.js
+++ cypress/integration/AddToCart/AddToCakrt.spec.js
@@ -11,12 +11,34 @@ When('Search the product starts with {string}',(keyword)=>{
 
 Then('Add {string} in the cart',(product)=>{
     cy.get('h4[class="product-name"]').each((el,index)=>{
-        //cy.log(typeof(el.text()))
-        let pro = el.text().replace('- 1 Kg','').trim()
-       // cy.log(pro)
-        cy.wait(3000)
+        cy.log(el.text())
+         let pro = el.text().replace('- 1 Kg','').trim()
+    //    // cy.log(pro)
+         //cy.wait(3000)
         if(pro == product){
             cy.get('.product-action >button').eq(index).click()
         }
     })
+})
+
+Then('Click on cart icon and Proceed to checkout',()=>{
+    cy.get('.cart-icon').click()
+    cy.get('.action-block >button').first().click()
+})
+
+And('Click on place order',()=>{
+    cy.contains('Place Order').click()
+})
+
+Then('Select country and check condition tab',()=>{
+    cy.get('select').select('India')
+    cy.get('[type="checkbox"]').check()
+})
+
+And('Click on Proceed',()=>{
+    cy.get('button').click()
+})
+
+And('Validate the Sucessfull msg',()=>{
+    cy.contains(`Thank you, your order has been placed successfully You'll be redirected to Home page shortly!!`).should('be.visible')
 })
